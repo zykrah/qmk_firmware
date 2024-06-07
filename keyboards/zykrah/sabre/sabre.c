6da5a690ba0190d68522233e59b589cc5d4e93b4
harky <23428162+zykrah@users.noreply.github.com>
2024-06-07 17:10:06 +1000

    Fixed LED positions and added code for indicators

--- keyboards/zykrah/sabre/sabre.c
+++ keyboards/zykrah/sabre/sabre.c
@@ -17,6 +17,20 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "quantum.h"
 
+void matrix_init_kb(void) {
+    gpio_set_pin_output(GP28);  // Caps Lock LED
+    gpio_set_pin_output(GP13);  // Scroll Lock LED
+}
+
+bool led_update_kb(led_t led_state) {
+    bool res = led_update_user(led_state);
+    if(res) {
+        gpio_write_pin(GP28, led_state.caps_lock);    // Caps Lock LED
+        gpio_write_pin(GP13, led_state.scroll_lock);  // Scroll Lock LED
+    }
+    return res;
+}
+
 #ifdef RGB_MATRIX_ENABLE
 led_config_t g_led_config = {
     {
@@ -34,7 +48,7 @@ led_config_t g_led_config = {
         { 48, 210}, { 32, 210}, { 16, 210},
         {  0, 210}, { 16, 196}, { 32, 182}, {  0, 168}, { 16, 154}, { 32, 140}, {  0, 126}, { 16, 112}, { 32,  98}, {  0,  70}, { 16,  56}, { 32,  48}, {  0,  28}, { 16,  14}, { 32,   0},
         { 16,   0}, { 32,   0}, { 48,   0},
-        {  0,  64}, { 14,  64}, { 28,  64}, { 42,  64},  { 56,  64}, { 70,  64}, { 84,  64}, { 98,  64}, {112,  64}, {126,  64}, {140,  64}, {154,  64}, {168,  64}, {182,  64}, {196,  64}, {210,  64}
+        { 64,   0}, { 64,  14}, { 64,  28}, { 64,  42},  { 64,  56}, { 64,  70}, { 64,  84}, { 64,  98}, { 64, 112}, { 64, 126}, { 64, 140}, { 64, 154}, { 64, 168}, { 64, 182}, { 64, 196}, { 64, 210}
     }, {
         // LED Index to Flag
         2, 2, 2,                                        // UNDERGLOW: RIGHT MIDDLE
